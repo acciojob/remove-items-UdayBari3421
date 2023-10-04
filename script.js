8cbec754be6043e4fdd463eb8c38dc4ffe89c72c
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-04 18:24:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button and dropdown elements
+    var button = document.querySelector('input[type="button"]');
+    var dropdown = document.getElementById('colorSelect');
+
+    // Add click event listener to the button
+    button.addEventListener('click', function () {
+        // Get the selected index
+        var selectedIndex = dropdown.selectedIndex;
+
+        // Check if any option is selected
+        if (selectedIndex !== -1) {
+            // Remove the selected option
+            dropdown.remove(selectedIndex);
+        }
+    });
+});
